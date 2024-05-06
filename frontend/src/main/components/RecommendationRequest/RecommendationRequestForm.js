23ed4cafd026b9211aa0ec9b06c9bc087b907f1d
Division7 <djensen2@outlook.com>
2024-05-05 23:39:30 -0700

    dj - Added the Recommendation Request Form, associated tests, and the proper exports in RecommendationRequestForm.stories.js

---
+++ frontend/src/main/components/RecommendationRequest/RecommendationRequestForm.js
@@ -0,0 +1,149 @@
+import { Button, Form, Row, Col } from 'react-bootstrap';
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+function RecommendationRequestForm({initialContents, submitAction, buttonLabel = "Create"}){
+    const{
+        register,
+        formState: {errors},
+        handleSubmit
+    } = useForm(
+        {defaultValues: initialContents || {},}
+    );
+
+    const navigate = useNavigate();
+
+    const date_regex = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/i;
+
+    const email_regex = /(\w)+@(\w)+\.(\w)+/i;
+
+    return (
+        <Form onSubmit={handleSubmit(submitAction)}>
+            <Row>
+                {initialContents && (
+                    <Col>
+                        <Form.Group className="mb-3">
+                            <Form.Label htmlFor="id">Id</Form.Label>
+                            <Form.Control
+                                data-testid="RecommendationRequestForm-id"
+                                id="id"
+                                type="text"
+                                {...register("id")}
+                                value={initialContents.id}
+                                disabled
+                            />
+                        </Form.Group>
+                    </Col>
+                )}
+
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="requesterEmail">Requester Email</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-requesterEmail"
+                            id="requesterEmail"
+                            type="text"
+                            isInvalid={Boolean(errors.requesterEmail)}
+                            {...register("requesterEmail", {required: true, pattern: email_regex})}
+                            />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.requesterEmail && 'Requester Email is required. '}
+                            {errors.requesterEmail?.type === 'pattern' && 'Requester Email must be email-format.'}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="professorEmail">Professor Email</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-professorEmail"
+                            id="professorEmail"
+                            type="text"
+                            isInvalid={Boolean(errors.professorEmail)}
+                            {...register("professorEmail", {required: true, pattern: email_regex})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.professorEmail && 'Professor Email is required. '}
+                            {errors.professorEmail?.type === 'pattern' && 'Professor Email must be email-format.'}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="explanation">Explanation</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-explanation"
+                            id="explanation"
+                            type="text"
+                            isInvalid={Boolean(errors.explanation)}
+                            {...register("explanation", {required: true})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.professorEmail && 'Explanation is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="dateRequested">Date Requested</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-dateRequested"
+                            id="dateRequested"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.dateRequested)}
+                            {...register("dateRequested", {required: true, pattern: date_regex})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.professorEmail && 'Date Requested is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="dateNeeded">Date Needed</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-dateNeeded"
+                            id="dateNeeded"
+                            type="datetime-local"
+                            isInvalid={Boolean(errors.dateNeeded)}
+                            {...register("dateNeeded", {required: true, pattern: date_regex})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.professorEmail && 'Date Needed is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" >
+                        <Form.Label htmlFor="completed">Completed</Form.Label>
+                        <Form.Control
+                            data-testid="RecommendationRequestForm-completed"
+                            id="completed"
+                            type="boolean"
+                            isInvalid={Boolean(errors.completed)}
+                            {...register("completed", {required: true})}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {errors.professorEmail && 'Completed is required. '}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button type="submit" data-testid="RecommendationRequestForm-submit">{buttonLabel}</Button>
+                    <Button
+                        variant="Secondary"
+                        onClick={() => navigate(-1)}
+                        data-testid="RecommendationRequestForm-cancel">
+                        Cancel
+                    </Button>
+                </Col>
+            </Row>
+        </Form>
+
+    );
+
+}
+
+export default RecommendationRequestForm;
---
+++ frontend/src/tests/components/RecommendationRequest/RecommendationRequestForm.test.js
@@ -0,0 +1,127 @@
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import RecommendationRequestForm from "main/components/RecommendationRequest/RecommendationRequestForm";
+import { recommendationRequestFixtures } from "fixtures/recommendationRequestFixtures";
+import { BrowserRouter as Router } from "react-router-dom";
+import UCSBDateForm from "../../../main/components/UCSBDates/UCSBDateForm";
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedNavigate
+}));
+
+describe("RecommendationRequestForm tests", () => {
+    test("renders correctly", async () => {
+        render(
+            <Router>
+                <RecommendationRequestForm />
+            </Router>
+        );
+        await screen.getByText(/Requester Email/);
+        await screen.getByText(/Create/);
+    });
+
+    test("renders correctly when passing in a RecommendationRequest", async () => {
+        render(
+            <Router>
+                <RecommendationRequestForm initialContents={recommendationRequestFixtures.oneRecommendationRequest} />
+            </Router>
+        );
+        await screen.findByTestId(/RecommendationRequestForm-id/);
+        expect(screen.getByText(/Id/)).toBeInTheDocument();
+        expect(screen.getByTestId(/RecommendationRequestForm-id/)).toHaveValue("1");
+    });
+
+    test("Correct error messages on bad input", async () => {
+        render(
+            <Router >
+                <RecommendationRequestForm />
+            </Router>
+        );
+        await screen.getByTestId("RecommendationRequestForm-requesterEmail");
+
+        const recommend = screen.getByTestId("RecommendationRequestForm-requesterEmail");
+        const prof = screen.getByTestId("RecommendationRequestForm-professorEmail");
+        const dateRequested = screen.getByTestId("RecommendationRequestForm-dateRequested");
+        const dateNeeded = screen.getByTestId("RecommendationRequestForm-dateNeeded");
+        const submitButton = screen.getByTestId("RecommendationRequestForm-submit");
+
+
+        fireEvent.change(recommend, {target: {value: 'bad-input'}});
+        fireEvent.change(prof, {target: {value: 'bad-input'}});
+        fireEvent.change(dateRequested, {target: {value: 'bad-input'}});
+        fireEvent.change(dateNeeded, {target: {value: 'bad-input'}});
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Requester Email must be email-format./);
+
+    });
+
+    test("error messages on missing input", async () => {
+        render(
+            <Router>
+                <RecommendationRequestForm />
+            </Router>
+        );
+        await screen.findByTestId("RecommendationRequestForm-submit");
+        const submitButton = screen.getByTestId("RecommendationRequestForm-submit");
+
+        fireEvent.click(submitButton);
+
+        await screen.findByText(/Requester Email is required./);
+        expect(screen.getByText(/Professor Email is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Explanation is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Date Requested is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Date Needed is required./)).toBeInTheDocument();
+        expect(screen.getByText(/Completed is required./)).toBeInTheDocument();
+    });
+
+    test("No Errors on good input", async () => {
+        const mockSubmitAction = jest.fn();
+
+        render(
+            <Router >
+                <RecommendationRequestForm submitAction={mockSubmitAction} />
+            </Router>
+        );
+
+        await screen.getByTestId("RecommendationRequestForm-requesterEmail");
+
+        const recommend = screen.getByTestId("RecommendationRequestForm-requesterEmail");
+        const prof = screen.getByTestId("RecommendationRequestForm-professorEmail");
+        const explanation = screen.getByTestId("RecommendationRequestForm-explanation");
+        const dateRequested = screen.getByTestId("RecommendationRequestForm-dateRequested");
+        const dateNeeded = screen.getByTestId("RecommendationRequestForm-dateNeeded");
+        const completed = screen.getByTestId("RecommendationRequestForm-completed");
+        const submitButton = screen.getByTestId("RecommendationRequestForm-submit");
+
+        fireEvent.change(recommend, {target: {value: 'djensen2@outlook.com'}});
+        fireEvent.change(prof, {target: {value: 'pconrad@ucsb.edu'}});
+        fireEvent.change(prof, {target: {value: 'masters program'}});
+        fireEvent.change(dateRequested, {target: {value: '2024-05-08T08:00:00'}});
+        fireEvent.change(dateNeeded, {target: {value: '2024-05-08T10:00:00'}});
+        fireEvent.change(dateNeeded, {target: {value: true}});
+        fireEvent.click(submitButton);
+
+        expect(screen.queryByText(/must be email-format/)).not.toBeInTheDocument();
+        expect(screen.queryByText(/must be in ISO format/)).not.toBeInTheDocument();
+    });
+
+
+    test("that navigate(-1) is called when Cancel is clicked", async () => {
+
+        render(
+            <Router  >
+                <RecommendationRequestForm />
+            </Router>
+        );
+        await screen.findByTestId("RecommendationRequestForm-cancel");
+        const cancelButton = screen.getByTestId("RecommendationRequestForm-cancel");
+
+        fireEvent.click(cancelButton);
+
+        await waitFor(() => expect(mockedNavigate).toHaveBeenCalledWith(-1));
+
+    });
+})
