d6703250435a4d2c584354810e1f935a8fcf796b
Jason Rodrigues <jason_rodrigues@ucsb.edu>
2024-05-08 12:41:41 -0700

    jr added help fixture

---
+++ frontend/src/fixtures/helpRequestFixtures.js
@@ -0,0 +1,43 @@
+const helpRequestFixtures = {
+    oneHelpRequest: {
+        "id": 1,
+        "requesterEmail": "example@ucsb.edu",
+        "teamId": "5pm-4",
+        "tableOrBreakoutRoom": "12",
+        "requestTime": "2022-01-02T12:00:00",
+        "explanation": "Issues",
+        "solved": true
+    },
+    threeHelpRequests: [
+        {
+            "id": 1,
+            "requesterEmail": "example1@ucsb.edu",
+            "teamId": "5pm-41",
+            "tableOrBreakoutRoom": "1",
+            "requestTime": "2022-01-02T12:00:00",
+            "explanation": "Issues",
+            "solved": true
+        },
+        {
+            "id": 2,
+            "requesterEmail": "example2@ucsb.edu",
+            "teamId": "5pm-42",
+            "tableOrBreakoutRoom": "2",
+            "requestTime": "2023-01-02T12:00:00",
+            "explanation": "Bugs",
+            "solved": true
+        },
+        {
+            "id": 3,
+            "requesterEmail": "example@ucsb.edu",
+            "teamId": "5pm-43",
+            "tableOrBreakoutRoom": "3",
+            "requestTime": "2024-01-02T12:00:00",
+            "explanation": "Broken",
+            "solved": true
+        }
+    ]
+};
+
+
+export { ucsbDatesFixtures };
