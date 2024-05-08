f3cce0064d35dc14745cd853e2567a7d9d66b7e5
Sreeganesh Siva <ssiva@ucsb.edu>
2024-05-07 18:15:14 -0700

    ss - fixed commas issue in the .js fixtures file

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,7 +1,7 @@
 const ucsbDatesFixtures = {
     oneDate: {
         "id": 1,
-        "itemId": 1
+        "itemId": 1,
         "stars": 1,
         "reviewerEmail": "test1@gmail.com",
         "localDateTime": "2022-01-02T12:00:00"
@@ -9,21 +9,21 @@ const ucsbDatesFixtures = {
     threeDates: [
         {
             "id": 1,
-            "itemId": 1
+            "itemId": 1,
             "stars": 1,
             "reviewerEmail": "test1@gmail.com",
             "localDateTime": "2022-01-01T12:00:00"
         },
         {
             "id": 2,
-            "itemId": 2
+            "itemId": 2,
             "stars": 2,
             "reviewerEmail": "test2@gmail.com",
             "localDateTime": "2022-01-02T12:00:00"
         },
         {
             "id": 3,
-            "itemId": 3
+            "itemId": 3,
             "stars": 3,
             "reviewerEmail": "test3@gmail.com",
             "localDateTime": "2022-01-03T12:00:00"
