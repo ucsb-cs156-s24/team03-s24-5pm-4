2e432334b7cb8c8731d4f646cf9de6ab52c5fc28
Sreeganesh Siva <ssiva@ucsb.edu>
2024-05-11 22:18:22 -0700

    ss - added create page, tests and stories file

--- frontend/src/tests/pages/MenuItemReview/MenuItemReviewCreatePage.test.js
+++ frontend/src/tests/pages/MenuItemReview/MenuItemReviewCreatePage.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import PlaceholderCreatePage from "main/pages/MenuItemReview/MenuItemReviewCreatePage";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import MenuItemReviewCreatePage from "main/pages/MenuItemReview/MenuItemReviewCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
@@ -8,36 +8,104 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
-describe("PlaceholderCreatePage tests", () => {
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
 
-    const axiosMock = new AxiosMockAdapter(axios);
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
 
-    const setupUserOnly = () => {
+describe("MenuItemReviewCreatePage tests", () => {
+
+    const axiosMock =new AxiosMockAdapter(axios);
+
+    beforeEach(() => {
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
+    });
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arrange
-
-        setupUserOnly();
-       
-        // act
+    test("renders without crashing", () => {
+        const queryClient = new QueryClient();
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
-                    <PlaceholderCreatePage />
+                    <MenuItemReviewCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+    });
+
+    test("on submit, makes request to backend, and redirects to /menuitemreviews", async () => {
+
+        const queryClient = new QueryClient();
+        const menuItemReview = {
+            id: 1,
+            itemId: 1,
+            reviewerEmail: "test@ucsb.edu",
+            stars: "4",
+            localDateTime: "2024-02-02T00:00",
+            comments: "test"
+        };
+
+        axiosMock.onPost("/api/menuitemreviews/post").reply( 202, menuItemReview );
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <MenuItemReviewCreatePage />
                 </MemoryRouter>
             </QueryClientProvider>
         );
 
-        // assert
-        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByTestId("MenuItemReviewForm-itemId")).toBeInTheDocument();
+        });
+
+        const itemIdField = screen.getByTestId("MenuItemReviewForm-itemId")
+        const reviewerEmailField = screen.getByTestId("MenuItemReviewForm-reviewerEmail");
+        const starsField = screen.getByTestId("MenuItemReviewForm-stars");
+        const dateReviewedField = screen.getByTestId("MenuItemReviewForm-dateReviewed");
+        const commentsField = screen.getByTestId("MenuItemReviewForm-comments");
+
+        const submitButton = screen.getByTestId("MenuItemReviewForm-submit");
+
+        fireEvent.change(itemIdField, { target: { value: '1' } });
+        fireEvent.change(reviewerEmailField, { target: { value: 'test@ucsb.edu' } });
+        fireEvent.change(starsField, { target: { value: '4' } });
+        fireEvent.change(dateReviewedField, { target: { value: '2024-02-02T00:00' } });
+        fireEvent.change(commentsField, { target: { value: 'test' } });
+
+        expect(submitButton).toBeInTheDocument();
+
+        fireEvent.click(submitButton);
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+        expect(axiosMock.history.post[0].params).toEqual(
+            {
+            "comments": "test",
+            "dateReviewed": "2024-02-02T00:00",
+            "stars": "4",
+            "reviewerEmail": "test@ucsb.edu",
+            "itemId": "1",
+        });
+
+        expect(mockToast).toBeCalledWith("New menuItemReview Created - id: 1 itemId: 1");
+        expect(mockNavigate).toBeCalledWith({ "to": "/menuitemreviews" });
     });
-
 });
-
-
