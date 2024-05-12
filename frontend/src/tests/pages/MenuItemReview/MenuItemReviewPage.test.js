e06073dbc3f2b350771798747e8ee8655547da13
Sreeganesh Siva <51520358+SR33G33@users.noreply.github.com>
2024-05-11 21:28:28 -0700

    Update MenuItemReviewPage.test.js

--- frontend/src/tests/pages/MenuItemReview/MenuItemReviewPage.test.js
+++ frontend/src/tests/pages/MenuItemReview/MenuItemReviewPage.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import PlaceholderIndexPage from "main/pages/Placeholder/MenuItemReviewIndexPage";
+import PlaceholderIndexPage from "main/pages/MenuItemReview/MenuItemReviewIndexPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
