e21c95a4f887b4d2d8e91561e9bdef9bad933456
Elijah Anderson <elijah_c_anderson@ucsb.edu>
2024-05-11 18:36:43 -0700

    Form setup

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,45 @@
+const articlesFixtures = {
+    oneArticles:
+    [
+      {
+       "id": 1,
+        "title": "Jump man can't jump",
+        "explanation": "Drake fails to make a basketball dunk",
+        "dateAdded" : "2022-07-04T12:00:00",      
+        "url" : "https://drakenews.com/",
+        "email" : "drakenews@gmail.com",
+      }
+    ],
+
+    threeArticles:
+    [
+        { "id": 1,
+        "title": "WHAAA",
+        "explanation": "KFC USING HORSE MEAT",
+        "dateAdded" : "2022-07-04T12:00:01",      
+        "url" : "https://fastfood.com/",
+        "email" : "mtvMilks@gmail.com",
+        },
+
+        {
+            "id": 2,
+            "title": "what do you mean?",
+            "explanation": "I do not know",
+            "dateAdded" : "2022-07-04T12:00:20",      
+            "url" : "https://none.com/",
+            "email" : "Nope@gmail.com",  
+        },
+
+        {
+            "id": 3,
+        "title": "I cannot watch",
+        "explanation": "Scary movies not to watch with family",
+        "dateAdded" : "2022-07-04T12:20:00",      
+        "url" : "https://movietrends.com/",
+        "email" : "movietrens@gmail.com",     
+        },
+        
+    ]
+};
+
+export { articlesFixtures };
