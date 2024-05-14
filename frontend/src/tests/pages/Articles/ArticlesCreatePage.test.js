328df84209d89eea7d676b8d25dac4577d092c9f
Elijah Anderson <elijah_c_anderson@ucsb.edu>
2024-05-13 22:56:06 -0700

    push for error checking

--- frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesCreatePage.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import ArticlesCreatePage from "Articles/ArticlesCreatePage";
+import { render, waitFor, fireEvent, screen } from "@testing-library/react";
+import ArticlesCreatePage from "main/pages/Articles/ArticlesCreatePage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
@@ -8,24 +8,39 @@ import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
+const mockToast = jest.fn();
+jest.mock('react-toastify', () => {
+    const originalModule = jest.requireActual('react-toastify');
+    return {
+        __esModule: true,
+        ...originalModule,
+        toast: (x) => mockToast(x)
+    };
+});
+
+const mockNavigate = jest.fn();
+jest.mock('react-router-dom', () => {
+    const originalModule = jest.requireActual('react-router-dom');
+    return {
+        __esModule: true,
+        ...originalModule,
+        Navigate: (x) => { mockNavigate(x); return null; }
+    };
+});
+
 describe("ArticlesCreatePage tests", () => {
 
-    const axiosMock = new AxiosMockAdapter(axios);
+    const axiosMock =new AxiosMockAdapter(axios);
 
-    const setupUserOnly = () => {
+    beforeEach(() => {
         axiosMock.reset();
         axiosMock.resetHistory();
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
         axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
-    };
+    });
 
-    const queryClient = new QueryClient();
-    test("Renders expected content", () => {
-        // arr ange
-
-        setupUserOnly();
-       
-        // act
+    test("renders without crashing", () => {
+        const queryClient = new QueryClient();
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -33,10 +48,38 @@ describe("ArticlesCreatePage tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
-
-        // assert
-        expect(screen.getByText("Create page not yet implemented")).toBeInTheDocument();
     });
+
+    test("on submit, makes request to backend, and redirects to /articles", async () => {
+
+        const queryClient = new QueryClient();
+        const articles = {
+            id: 3,
+            title: "nah",
+            explanation: "Bad bad bad",
+            email: "123milkz@gmail.com",
+            url: "123milkz.com",
+            dateAdded: "2024-05-14T04:17:14Z"
+
+        };
+
+        axiosMock.onPost('/api/articles/post').reply(202, articles);
+
+        render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <ArticlesCreatePage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        )
+
+
+        await waitFor(() => expect(axiosMock.history.post.length).toBe(1));
+
+
+
+    });
+
 
 });
 
