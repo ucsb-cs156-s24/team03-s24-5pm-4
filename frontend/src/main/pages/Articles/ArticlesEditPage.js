66eff56c05037e99cc6b864ec1655e694b687b7e
Elijah Anderson <elijah_c_anderson@ucsb.edu>
2024-05-14 01:55:08 -0700

    Edit implementation with mutation coverage

--- frontend/src/main/pages/Articles/ArticlesEditPage.js
+++ frontend/src/main/pages/Articles/ArticlesEditPage.js
@@ -1,13 +1,72 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import ArticlesForm from "main/components/Articles/ArticlesForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function ArticlesEditPage() {
+export default function ArticlesEditPage({storybook=false}) {
+  let { id } = useParams();
 
-  // Stryker disable all : placeholder for future implementation
+  const { data: articles, _error, _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      [`/api/articles?id=${id}`],
+      {  // Stryker disable next-line all : GET is the default, so changing this to "" doesn't introduce a bug
+        method: "GET",
+        url: `/api/articles`,
+        params: {
+          id
+        }
+      }
+    );
+
+
+  const objectToAxiosPutParams = (articles) => ({
+    url: "/api/articles",
+    method: "PUT",
+    params: {
+      id: articles.id,
+    },
+    data: {
+      title: articles.title,
+      dateAdded: articles.dateAdded,
+      url: articles.url,
+      explanation: articles.explanation,
+      email: articles.email
+    }
+  });
+
+  const onSuccess = (articles) => {
+    toast(`Articlearticles Updated - id: ${articles.id} title: ${articles.title}`);
+  }
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/articles?id=${id}`]
+  );
+
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/articles" />
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit Articles</h1>
+        {
+          articles && <ArticlesForm initialContents={articles} submitAction={onSubmit} buttonLabel="Update" />
+        }
       </div>
     </BasicLayout>
   )
 }
+
