2f833734f6d66d8295eb76fd672322e40af0ac08
jason-rodrigues <97640916+jason-rodrigues@users.noreply.github.com>
2024-06-03 19:27:33 -0700

    Update AppNavbar.js
    Duplicate HelpRequest in Nav Bar appeared when we had to rebase my open pull request.

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -64,7 +64,6 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   <Nav.Link as={Link} to="/helprequest">HelpRequest</Nav.Link>
                   <Nav.Link as={Link} to="/menuitemreview">MenuItemReview</Nav.Link>
                   <Nav.Link as={Link} to="/recommendationrequest">Recommendation Requests</Nav.Link>
-                  <Nav.Link as={Link} to="/helprequest">HelpRequest</Nav.Link>
                 </>
               )
             }
